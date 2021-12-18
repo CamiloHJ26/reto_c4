f79dcf4f8592f0c5246a33eff5d832ca9575b802
Camilohj26 <camilohj213@gmail.com>
2021-12-17 20:05:34 -0500

    cambio mongo

--- src/main/java/com/example/Reto3Jeremy1/controller/OrderController.java
+++ src/main/java/com/example/Reto3Jeremy1/controller/OrderController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.example.Reto3Jeremy1.controller;
 
 import com.example.Reto3Jeremy1.model.Order;
@@ -22,10 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Jeremy
- */
+
 @RestController
 @RequestMapping("/api/order")
 @CrossOrigin("*")
